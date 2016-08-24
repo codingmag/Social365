73cc0c3879b98e582eddde557c4d0010db5c3296
codingmag <mbulutay@gmail.com>
2016-08-24 16:02:19 +0300

    Add project files.

---
+++ SocialFeedCollector.Job/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialFeedCollector.Job
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (string.IsNullOrEmpty(ConfigurationManager.AppSettings["SharePointSiteUrl"]))
+            {
+                throw new ArgumentNullException("SharePointSiteUrl");
+            }
+
+            var siteUri = new Uri(ConfigurationManager.AppSettings["SharePointSiteUrl"]);
+            
+            //Get the realm for the URL
+            var realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+
+            // Get the access token for the URL.  
+            //   Requires this app to be registered with the tenant
+            var accessToken = TokenHelper.GetAppOnlyAccessToken(
+                TokenHelper.SharePointPrincipal,
+                siteUri.Authority, realm).AccessToken;
+
+            // Get client context with access token
+            using (var clientContext =
+                TokenHelper.GetClientContextWithAccessToken(
+                    siteUri.ToString(), accessToken))
+            {
+                //Todo: get feeds and insert to list...
+            }
+        }
+    }
+}
